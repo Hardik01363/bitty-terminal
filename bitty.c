e29d8d4c73a7b01bd40d1cc804ed128270169523
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-07-03 11:41:32 +0530

    execlp implemented

--- bitty.c
+++ bitty.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <pty.h>
+
+static int32_t masterfd;
 
 int main(void) {
+    if(forkpty(&masterfd, NULL, NULL, NULL) == 0) {
+        execlp("usr/bin/bash", "bash", NULL);
+        perror("execlp");
+        exit(1);
+    }
+
+    bool running = true;
+
+    while(running) {
+        
+    }
+
     printf("Hello World!");
     return EXIT_SUCCESS;
 }
