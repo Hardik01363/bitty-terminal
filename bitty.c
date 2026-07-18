958909a41ff6cd39207fce82c471b3a3e81cefd2
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-07-18 23:40:51 +0530

    utf8_encode copied and modified

--- bitty.c
+++ bitty.c
@@ -17,7 +17,30 @@
 
 static int32_t masterfd;
 
-int32_t decode_utf8(const char *s, uint32_t  *out_cp) {// Returns length of utf8 string
+typedef struct {
+    uint32_t codepoint;
+} cell_t;
+
+typedef struct {
+    uint32_t x,y;
+} cursor_t;
+
+static cursor_t cursor;
+static cell_t* cells;
+static uint32_t rows = 30;
+static uint32_t cols = 100;
+static lf_ui_state_t* ui;
+
+void setcell(uint32_t x, uint32_t y, uint32_t codepoint) {
+    cells[x*cols + y].codepoint = codepoint; //mapping 2D coords to 1D
+}
+
+void handle_codepoint(uint32_t codepoint) {
+    setcell(cursor.x, cursor.y, codepoint);
+    cursor.x++;
+}
+
+int32_t utf8_decode(const char *s, uint32_t  *out_cp) {// Returns length of utf8 string
     unsigned char c = s[0];
     if (c < 0x80) {
         *out_cp = c;
@@ -35,34 +58,66 @@ int32_t decode_utf8(const char *s, uint32_t  *out_cp) {// Returns length of utf8
     return -1; // invalid UTF-8
 }
 
+int32_t utf8_encode(uint32_t codepoint, char *out) {
+    if(codepoint <= 0x7F) {
+        out[0] = codepoint;
+        return 1;
+    }
+    else if(codepoint <= 0x7FF) {
+        out[0] = 0xC0 | (codepoint >> 6);
+        out[1] = 0x80 | (codepoint & 0x3F);
+        return 2;
+    }
+    else if(codepoint <= 0xFFFF) {
+        out[0] = 0xE0 | (codepoint >> 12);
+        out[1] = 0x80 | ((codepoint >> 6) & 0x3F);
+        out[2] = 0x80 | (codepoint & 0x3F);
+        return 3;
+    }
+    else if(codepoint <= 0x10FFFF) {
+        out[0] = 0xF0 | (codepoint >> 18);
+        out[1] = 0x80 | ((codepoint >> 12) & 0x3F);
+        out[2] = 0x80 | ((codepoint >> 6) & 0x3F);
+        out[3] = 0x80 | (codepoint & 0x3F);
+        return 4;
+    }
+    return 0;
+}
+
+void render_rows() {
+    for(uint32_t i = 0; i < rows; i++) {
+        char* row = malloc(cols * sizeof(uint32_t) + 1); //uint32_t as we are hoping to support unicode
+        ui->render_text(ui->render_state)
+    }
+}
+
 //only useful parts of the function lf_ui_core_next_event() modified from ../bitty-terminal/reif/src/ui_core.c
 void term_next_event(lf_ui_state_t* ui) {
-  float cur_time = lf_ui_core_get_elapsed_time();
-  ui->delta_time = cur_time - ui->_last_time;
-  ui->_last_time = cur_time;
+    float cur_time = lf_ui_core_get_elapsed_time();
+    ui->delta_time = cur_time - ui->_last_time;
+    ui->_last_time = cur_time;
 
-  bool rendered = lf_windowing_get_current_event() == LF_EVENT_WINDOW_REFRESH;
-  lf_ui_core_shape_widgets_if_needed(ui, ui->root, false);
+    bool rendered = lf_windowing_get_current_event() == LF_EVENT_WINDOW_REFRESH;
+    lf_ui_core_shape_widgets_if_needed(ui, ui->root, false);
 
-  if(ui->needs_render) {
-    lf_win_make_gl_context(ui->win);
-    vec2s winsize = lf_win_get_size(ui->win);
-    ui->render_clear_color_area(lf_color_from_hex(0xa03cf2), LF_SCALE_CONTAINER(winsize.x, winsize.y), winsize.y);
-    ui->render_begin(ui->render_state);
-    ui->render_rect(ui->render_state, (vec2s){50, 50}, (vec2s){50, 50}, LF_RED, LF_NO_COLOR, 0.0f, 0.0f);
-    ui->render_end(ui->render_state);
-    lf_win_swap_buffers(ui->win);
-    ui->needs_render = false;
-    rendered = true;
-  }
+    if(ui->needs_render) {
+        lf_win_make_gl_context(ui->win);
+        vec2s winsize = lf_win_get_size(ui->win);
+        ui->render_clear_color_area(lf_color_from_hex(0xa03cf2), LF_SCALE_CONTAINER(winsize.x, winsize.y), winsize.y);
+        ui->render_begin(ui->render_state);
+        ui->render_rect(ui->render_state, (vec2s){50, 50}, (vec2s){50, 50}, LF_RED, LF_NO_COLOR, 0.0f, 0.0f);
+        ui->render_end(ui->render_state);
+        lf_win_swap_buffers(ui->win);
+        ui->needs_render = false;
+        rendered = true;
+    }
 
-  lf_windowing_update();
+    lf_windowing_update();
 }
 
 size_t read_from_pty(void) {
     static char buff[SHRT_MAX];
     static uint32_t buff_len = 0;
-
     int32_t read_bytes = read(masterfd, buff + buff_len, sizeof(buff) - buff_len);
     if(read_bytes <= 0) return 0;
     buff_len += read_bytes;
@@ -70,8 +125,9 @@ size_t read_from_pty(void) {
     uint32_t iter = 0;
     while(iter < buff_len) {
         uint32_t codepoint;
-        int32_t len = decode_utf8(&buff[iter], &codepoint);
+        int32_t len = utf8_decode(&buff[iter], &codepoint);
         if(len == -1 || len > buff_len) break;
+        handle_codepoint(codepoint);
         iter += len;
     }
 
@@ -90,9 +146,11 @@ int main(void) {
         exit(1);
     }
 
+    cells = malloc(sizeof(cell_t)*rows*cols);
+
     lf_windowing_init();
     lf_window_t window = lf_ui_core_create_window(1560, 980, "bitty - custom terminal & shell"); //parameters for lf_ui_core_create_window(pixel width, pixel height, terminal title)
-    lf_ui_state_t* ui = lf_ui_core_init(window); //initialising UI state
+    ui = lf_ui_core_init(window); //initialising UI state
 
     fd_set fdset;
     int32_t x11fd = ConnectionNumber(lf_win_get_x11_display());
